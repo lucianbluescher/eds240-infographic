914dde532d4b04c4d1d21a13513f075f1bf06ad9
Lucian Scher <lucian@bren.ucsb.edu>
2026-02-04 11:32:18 -0800

    submit FPM2

---
+++ spaghetti.R
@@ -0,0 +1,15 @@
+hydropower_generation <- read.csv("data/hydropower-generation/hydropower-generation.csv")
+
+per_capita_energy_stacked <- read.csv("data/per-capita-energy-stacked/per-capita-energy-stacked.csv")
+
+per_capita_greenhouse_gas_emissions <- read.csv("data/per-capita-greenhouse-gas-emissions/per-capita-greenhouse-gas-emissions.csv")
+
+per_capita_methane_emissions <- read.csv("data/per-capita-methane-emissions/per-capita-methane-emissions.csv")
+
+share_electricity_hydro <- read.csv("data/share-electricity-hydro/share-electricity-hydro.csv")
+
+summary(hydropower_generation)
+summary(per_capita_energy_stacked)
+summary(per_capita_greenhouse_gas_emissions)
+summary(per_capita_methane_emissions)
+summary(share_electricity_hydro)
